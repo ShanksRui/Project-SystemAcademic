c3f6c789ceb7fd9d16111667363dc3964db51be3
Luiz Felipe Vianna Vieira <luiz.vianna@MDIC.COLABORAGOV.GOV.BR>
2025-06-26 14:46:19 -0300

    implemented StudentsService/Resource and testing in postman

--- src/main/java/com/dev/System_Academic/Resources/StudentResource.java
+++ src/main/java/com/dev/System_Academic/Resources/StudentResource.java
@@ -5,6 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,5 +26,14 @@ public class StudentResource {
 		List<Student> students = service.findAll();
 		return ResponseEntity.ok().body(students);
 	}
-	
+	@GetMapping("/{id}")
+	public ResponseEntity<Student> findById(@PathVariable Long id){
+		Student obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	@PutMapping("/{id}")
+	public ResponseEntity<Student> update(@PathVariable Long id, @RequestBody Student student){
+		Student obj = service.Update(id, student);
+		return ResponseEntity.ok().body(obj);
+	}
 }
--- src/main/java/com/dev/System_Academic/Services/StudentService.java
+++ src/main/java/com/dev/System_Academic/Services/StudentService.java
@@ -1,6 +1,7 @@
 package com.dev.System_Academic.Services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,5 +17,25 @@ public class StudentService {
 	
 	public List<Student> findAll() {
 		return repository.findAll();
+	}
+	
+	public Student findById(Long id) {
+		Optional<Student> obj = repository.findById(id);
+		return obj.get();
+	}
+	public void deleteById(Long id) {
+		repository.deleteById(id);
+	}
+	public Student Update(Long id,Student student) {
+		Student entity = repository.getReferenceById(id);
+		updateStudent(entity,student);
+		return repository.save(entity);
+	}
+
+	private void updateStudent(Student entity, Student student) {
+		entity.setName(student.getName());
+		entity.setPaymentCourse(student.getPaymentCourse());
+		entity.setDatePayment(student.getdatePayment());
+		
 	}
 }
